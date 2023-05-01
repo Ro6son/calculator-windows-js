272132e5023c84106cc9fdef781d20e81df896e7
Robson Moraiz <morais.nosbor@gmail.com>
2023-05-01 16:09:06 -0300

    Melhorando a legibilidade e performance do código

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project: Calculator Windows
+
+## About:
+
+This HTML, CSS and JavaScript code implements a simple calculator. The interface is built with HTML, using classes and IDs to select the elements of interest. The CSS style file is used to define the appearance of the calculator's buttons. The JavaScript code defines the calculator's operating logic, using DOM element selectors to interact with the interface and implementing auxiliary functions to process the calculations.
+
+The 'display' variable is used to select the HTML element that displays the result of the operation. The remaining variables select the calculator buttons.
+
+The state of the calculator is held in variables such as 'first', 'operator', 'previousNumber' and 'currentNumber'. The 'updateDisplay' function is responsible for updating the content displayed on the calculator's display.
+
+Auxiliary functions, such as 'adjustCommaPoint', 'cleanDisplay', 'calculate', 'deleteLast', 'invertSign', 'calcPorc', 'calcDivisionOne' and 'calcPower', are used to perform the mathematical operations of the calculator. These functions are called according to the buttons suffered by the user.
+
+In summary, the code implements a calculator that allows you to perform simple mathematical operations, such as addition, subtraction, multiplication, division, calculated percentage, square root, power and sign inversion.
+
+## Concepts - Learning:
+
+1 - Simplifying the logic:
+The code has several functions and conditionals that could be simplified to reduce the size and complexity of the code, for example, instead of having a function for each calculator button, there could be a single function to handle all the buttons, using information from the attributes of each button to determine the corresponding action.
+2 - Use more modern resources:
+the code uses old JavaScript resources, such as the eval() method, which can be replaced by other safer and more efficient alternatives, such as Function(). In addition, it would be possible to use more modern features of JavaScript, such as asynchronous functions and arrow functions syntax.
+3 - Improve code readability:
+the code could be better organized and documented to make it easier and more efficient to read and maintain. This includes using consistent formatting, choosing more descriptive variable and function names, and adding explanatory comments.
+
+Note: Using libraries and frameworks:
+there are several JavaScript libraries and frameworks that could be used to simplify calculator development and make it more efficient. For example, libraries like React or Vue.js allow you to create user interfaces faster and more efficiently, while libraries like math.js can be used to simplify complex math calculations.
+
+
+
--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
+// Seleção dos elementos do DOM
 const display = document.getElementById("display");
-const displayText = document.getElementById("display").textContent;
-const equal = document.getElementById("equal");
+const igual = document.getElementById("igual");
 const apagar = document.getElementById("apagar");
 const maismenos = document.getElementById("maismenos");
 const limparCalculo = document.getElementById("limparCalculo");
@@ -12,149 +12,322 @@ const potencia = document.getElementById("potencia");
 const raiz = document.getElementById("raiz");
 
 const nums = document.querySelectorAll("[id*=num]");
-const operatores = document.querySelectorAll("[id*=sinal]");
+const operadores = document.querySelectorAll("[id*=sinal]");
 
-let first = true;
-let operator;
-let lastNum;
-let currentNum;
-let cleanEqual;
+// Declaração de variáveis / Estado
+let primeiro = true;
+let operador;
+let numeroAnterior = "0";
+let numeroAtual = "0";
 
-const commaTopoint = () =>
-  (display.textContent = display.textContent.replace(".", ","));
-
-const insertDisplay = (text) => {
-  if (first) {
-    display.textContent = text;
-    first = false;
-  } else {
-    display.textContent += text;
-  }
-
-  display.textContent = display.textContent.substring(0, 17);
-  currentNum = display.textContent;
-  cleanEqual = true;
+// Funções auxiliares
+const ajustaPontoVirgula = () => {
+  display.textContent = display.textContent.replace(".", ",");
 };
 
-const insert = (e) => insertDisplay(e.target.textContent);
-
-nums.forEach((e) => e.addEventListener("click", insert));
-
-const insertoperator = (e) => {
-  first = true;
-  operator = e.target.textContent;
-  if (operator == "x") {
-    operator = "*";
-  } else if (operator == "÷") {
-    operator = "/";
-  }
-  lastNum = display.textContent;
+const atualizaDisplay = (valor) => {
+  display.textContent = valor;
+  numeroAtual = display.textContent;
 };
 
-operatores.forEach((e) => e.addEventListener("click", insertoperator));
+const limpaDisplay = () => {
+  atualizaDisplay("0");
+  primeiro = true;
+};
 
 const calcular = () => {
-  if (lastNum && operator) {
-    let result = lastNum + operator;
-
-    if (currentNum) {
-      result += currentNum;
-    } else {
-      result += lastNum;
+  if (numeroAnterior && operador) {
+    let result = `${numeroAnterior}${operador}${numeroAtual || numeroAnterior}`;
+    let resultadoFinal = eval(result.replace(",", ".")).toString();
+    if (resultadoFinal === "NaN") {
+      resultadoFinal = "0";
     }
-
-    display.textContent = eval(result.replace(",", "."));
-    commaTopoint();
-
-    if (display.textContent == "NaN") {
-      display.textContent = "0";
-    }
-
-    lastNum = display.textContent;
-    first = true;
-    cleanEqual = false;
+    atualizaDisplay(resultadoFinal);
+    numeroAnterior = resultadoFinal;
+    primeiro = true;
   }
 };
-
-equal.addEventListener("click", calcular);
 
 const apagarUltimo = () => {
-  if (cleanEqual) {
-    if (display.textContent.length > 1) {
-      display.textContent = display.textContent.slice(0, -1);
-    } else {
-      display.textContent = 0;
-    }
-
-    first = true;
+  if (display.textContent.length > 1) {
+    atualizaDisplay(display.textContent.slice(0, -1));
+  } else {
+    limpaDisplay();
   }
 };
 
-apagar.addEventListener("click", apagarUltimo);
-
 const inverteSinal = () => {
-  display.textContent = parseFloat(display.textContent.replace(",", ".")) * -1;
-  commaTopoint();
+  atualizaDisplay(parseFloat(display.textContent.replace(",", ".")) * -1);
 };
-
-maismenos.addEventListener("click", inverteSinal);
-
-const cleanCalc = () => {
-  display.textContent = "0";
-  first = true;
-};
-
-limparCalculo.addEventListener("click", cleanCalc);
-
-const limpaTudo = () => {
-  display.textContent = "0";
-  lastNum = "0";
-  currentNum = "0";
-  first = true;
-};
-
-limparTudo.addEventListener("click", limpaTudo);
 
 const calcPorc = () => {
-  display.textContent = parseFloat(display.textContent.replace(",", ".")) / 100;
-  commaTopoint();
-  currentNum = display.textContent;
-  first = true;
+  atualizaDisplay(parseFloat(display.textContent.replace(",", ".")) / 100);
+  primeiro = true;
 };
-
-porc.addEventListener("click", calcPorc);
 
 const calcDivisaoUm = () => {
-  display.textContent = 1 / parseFloat(display.textContent.replace(",", "."));
-  commaTopoint();
-  currentNum = display.textContent;
-  lastNum = "0";
-  first = true;
+  atualizaDisplay(1 / parseFloat(display.textContent.replace(",", ".")));
+  numeroAnterior = "0";
+  primeiro = true;
 };
-
-divisaoUm.addEventListener("click", calcDivisaoUm);
 
 const calcPotencia = () => {
-  display.textContent = Math.pow(
-    parseFloat(display.textContent.replace(",", ".")),
-    2
+  atualizaDisplay(
+    Math.pow(parseFloat(display.textContent.replace(",", ".")), 2).toString()
   );
-  commaTopoint();
-  currentNum = display.textContent;
-  lastNum = "0";
-  first = true;
+  numeroAnterior = "0";
+  primeiro = true;
 };
-
-potencia.addEventListener("click", calcPotencia);
 
 const calcRaiz = () => {
-  display.textContent = Math.sqrt(
-    parseFloat(display.textContent.replace(",", "."))
+  atualizaDisplay(
+    Math.sqrt(parseFloat(display.textContent.replace(",", "."))).toString()
   );
-  commaTopoint();
-  currentNum = display.textContent;
-  lastNum = "0";
-  first = false;
+  numeroAnterior = "0";
+  primeiro = true;
 };
 
-raiz.addEventListener("click", calcRaiz);
+// Adição de listeners aos elementos do DOM
+nums.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (primeiro) {
+      atualizaDisplay(num.textContent);
+      primeiro = false;
+    } else {
+      atualizaDisplay(`${display.textContent}${num.textContent}`);
+    }
+    display.textContent = display.textContent.substring(0, 17);
+  });
+});
+
+operadores.forEach((operadorBtn) => {
+  operadorBtn.addEventListener("click", () => {
+    operador = operadorBtn.textContent;
+    if (operador === "x") {
+      operador = "*";
+    } else if (operador === "÷") {
+      operador = "/";
+    }
+    numeroAnterior = display.textContent;
+    primeiro = true;
+  });
+});
+// Adição de listeners aos botões de ação do calculadora
+igual.addEventListener("click", () => {
+  calcular();
+  ajustaPontoVirgula();
+});
+
+apagar.addEventListener("click", () => {
+  apagarUltimo();
+});
+
+maismenos.addEventListener("click", () => {
+  inverteSinal();
+});
+
+limparCalculo.addEventListener("click", () => {
+  atualizaDisplay("0");
+  primeiro = true;
+});
+
+limparTudo.addEventListener("click", () => {
+  atualizaDisplay("0");
+  operador = undefined;
+  numeroAnterior = "0";
+  primeiro = true;
+});
+
+porc.addEventListener("click", () => {
+  calcPorc();
+});
+
+virg.addEventListener("click", () => {
+  if (display.textContent.indexOf(".") === -1) {
+    atualizaDisplay(`${display.textContent}.`);
+  }
+});
+
+divisaoUm.addEventListener("click", () => {
+  calcDivisaoUm();
+});
+
+potencia.addEventListener("click", () => {
+  calcPotencia();
+});
+
+raiz.addEventListener("click", () => {
+  calcRaiz();
+});
+
+//End
+
+// # Project: Calculator Windows - https://github.com/WilliamDosSantos/Calculadora
+
+// const display = document.getElementById("display");
+// const displayText = document.getElementById("display").textContent;
+// const igual = document.getElementById("igual");
+// const apagar = document.getElementById("apagar");
+// const maismenos = document.getElementById("maismenos");
+// const limparCalculo = document.getElementById("limparCalculo");
+// const limparTudo = document.getElementById("limparTudo");
+// const porc = document.getElementById("porc");
+// const virg = document.getElementById("virg");
+// const divisaoUm = document.getElementById("divisaoUm");
+// const potencia = document.getElementById("potencia");
+// const raiz = document.getElementById("raiz");
+
+// const nums = document.querySelectorAll("[id*=num]");
+// const operadores = document.querySelectorAll("[id*=sinal]");
+
+// let primeiro = true;
+// let operador;
+// let numeroAnterior;
+// let numeroAtual;
+// let apagarIgual;
+
+// const ajustaPontoVirgula = () =>
+//   (display.textContent = display.textContent.replace(".", ","));
+
+// const inserirDisplay = (text) => {
+//   if (primeiro) {
+//     display.textContent = text;
+//     primeiro = false;
+//   } else {
+//     display.textContent += text;
+//   }
+
+//   display.textContent = display.textContent.substring(0, 17);
+//   numeroAtual = display.textContent;
+//   apagarIgual = true;
+// };
+
+// const inserir = (e) => inserirDisplay(e.target.textContent);
+
+// nums.forEach((e) => e.addEventListener("click", inserir));
+
+// const inserirOperador = (e) => {
+//   primeiro = true;
+//   operador = e.target.textContent;
+//   if (operador == "x") {
+//     operador = "*";
+//   } else if (operador == "÷") {
+//     operador = "/";
+//   }
+//   numeroAnterior = display.textContent;
+// };
+
+// operadores.forEach((e) => e.addEventListener("click", inserirOperador));
+
+// const calcular = () => {
+//   if (numeroAnterior && operador) {
+//     let result = numeroAnterior + operador;
+
+//     if (numeroAtual) {
+//       result += numeroAtual;
+//     } else {
+//       result += numeroAnterior;
+//     }
+
+//     display.textContent = eval(result.replace(",", "."));
+//     ajustaPontoVirgula();
+
+//     if (display.textContent == "NaN") {
+//       display.textContent = "0";
+//     }
+
+//     numeroAnterior = display.textContent;
+//     primeiro = true;
+//     apagarIgual = false;
+//   }
+// };
+
+// igual.addEventListener("click", calcular);
+
+// const apagarUltimo = () => {
+//   if (apagarIgual) {
+//     if (display.textContent.length > 1) {
+//       display.textContent = display.textContent.slice(0, -1);
+//     } else {
+//       display.textContent = 0;
+//     }
+
+//     primeiro = true;
+//   }
+// };
+
+// apagar.addEventListener("click", apagarUltimo);
+
+// const inverteSinal = () => {
+//   display.textContent = parseFloat(display.textContent.replace(",", ".")) * -1;
+//   ajustaPontoVirgula();
+// };
+
+// maismenos.addEventListener("click", inverteSinal);
+
+// const limpaCalculo = () => {
+//   display.textContent = "0";
+//   primeiro = true;
+// };
+
+// limparCalculo.addEventListener("click", limpaCalculo);
+
+// const limpaTudo = () => {
+//   display.textContent = "0";
+//   numeroAnterior = "0";
+//   numeroAtual = "0";
+//   primeiro = true;
+// };
+
+// limparTudo.addEventListener("click", limpaTudo);
+
+// const calcPorc = () => {
+//   display.textContent = parseFloat(display.textContent.replace(",", ".")) / 100;
+//   ajustaPontoVirgula();
+//   numeroAtual = display.textContent;
+//   primeiro = true;
+// };
+
+// porc.addEventListener("click", calcPorc);
+
+// const calcDivisaoUm = () => {
+//   display.textContent = 1 / parseFloat(display.textContent.replace(",", "."));
+//   ajustaPontoVirgula();
+//   numeroAtual = display.textContent;
+//   numeroAnterior = "0";
+//   primeiro = true;
+// };
+
+// divisaoUm.addEventListener("click", calcDivisaoUm);
+
+// const calcPotencia = () => {
+//   display.textContent = Math.pow(
+//     parseFloat(display.textContent.replace(",", ".")),
+//     2
+//   );
+//   ajustaPontoVirgula();
+//   numeroAtual = display.textContent;
+//   numeroAnterior = "0";
+//   primeiro = true;
+// };
+
+// potencia.addEventListener("click", calcPotencia);
+
+// const calcRaiz = () => {
+//   display.textContent = Math.sqrt(
+//     parseFloat(display.textContent.replace(",", "."))
+//   );
+//   ajustaPontoVirgula();
+//   numeroAtual = display.textContent;
+//   numeroAnterior = "0";
+//   primeiro = true;
+// };
+
+// raiz.addEventListener("click", calcRaiz);
+
+// virg.addEventListener("click", () => {
+//   if (display.textContent.indexOf(",") === -1) {
+//     inserirDisplay(".");
+
+//   }
+// });
